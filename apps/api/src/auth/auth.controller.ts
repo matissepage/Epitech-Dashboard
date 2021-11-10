ed4d625888b2d860d6ba2a94ce03b1d2014e8ae3
Clem <clement.bolin@epitech.eu>
2021-11-10 16:05:24 +0100

    add(feat/auth_5): add swagger configuration

--- apps/api/src/auth/auth.controller.ts
+++ apps/api/src/auth/auth.controller.ts
@@ -1,7 +1,10 @@
 import { Controller, Get } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private authService: AuthService) {}
+
   @Get('/google')
   googleAuth(): string {
     return 'googleAuth function'
