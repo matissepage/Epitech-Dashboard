d5f9a1e493eed71de26ad618c626234ff79474a5
Clem <clement.bolin@epitech.eu>
2021-11-11 23:14:24 +0100

    add(feat/auth_5): create user table

---
+++ sql/create_tables.sql
@@ -0,0 +1,10 @@
+--- Create User table ---
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
