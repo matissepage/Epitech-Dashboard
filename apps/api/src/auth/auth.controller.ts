435e9ec2456c73f5431259ae67085deb4869c79b
Clem <clement.bolin@epitech.eu>
2021-11-13 00:23:44 +0100

    add(feat/auth_5): google auth

--- apps/api/src/auth/auth.controller.ts
+++ apps/api/src/auth/auth.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth.service';
+import { Request } from 'express';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Get('/google')
-  googleAuth(): string {
-    return 'googleAuth function'
+  @UseGuards(AuthGuard('google'))
+  async googleAuth(@Req() req: Request) {}
+
+  @Get('redirect')
+  @UseGuards(AuthGuard('google'))
+  googleAuthCallback(@Req() req: Request) {
+    // or `return req.get('/google/redirect');`
+    return this.authService.googleLogin(req);
   }
 }
--- apps/api/src/auth/auth.service.ts
+++ apps/api/src/auth/auth.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { Request } from 'express';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  googleLogin(req: Request): string | {message: string, user: unknown} {
+    if (!req.user)
+      return 'no user from Google';
+    return {
+      message: 'User information from googlee',
+      user: req.user,
+    };
+  }
+}
