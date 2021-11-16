f93f171668a894d8f53ecc4038491150a1de3997
Clem <clement.bolin@epitech.eu>
2021-11-16 17:02:49 +0100

    fix(feat/local_auth): rxjs downgrade

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from '../auth/auth.module';
+import { UsersModule } from '../users/users.module';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [AuthModule],
+  imports: [TypeOrmModule.forRoot({}), AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
