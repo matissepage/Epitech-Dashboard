6d262ec58da92ed1bd0ba904d9023820de0f58d6
Clem <clement.bolin@epitech.eu>
2021-11-29 18:10:28 +0100

    add(feat/spotify_widget): spotify search route

--- apps/api/src/spotify/spotify.controller.ts
+++ apps/api/src/spotify/spotify.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { SpotifyService } from './spotify.service';
 
 @Controller('spotify')
-export class SpotifyController {}
+export class SpotifyController {
+  constructor(private spotifyService: SpotifyService) {}
+
+  @Get('/search/:artist/:type/:token')
+  spotifySearch(
+    @Param('artist') id: string,
+    @Param('token') token: string,
+    @Param('type') type: string,
+  ) {
+    return this.spotifyService.searchArtist(id, token, type);
+  }
+}
--- apps/api/src/spotify/spotify.service.ts
+++ apps/api/src/spotify/spotify.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { SpotifyArtist } from './../../../../shared/spotify.model';
+import { HttpService } from '@nestjs/axios';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable()
-export class SpotifyService {}
+export class SpotifyService {
+  constructor(private httpService: HttpService) {}
+
+  searchArtist(artist: string, token: string, type: string): Observable<SpotifyArtist[]> {
+    return this.httpService
+      .get(
+        `https://api.spotify.com/v1/search?q=${artist}&type=${type}&limit=20`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        },
+      )
+      .pipe(map(response => {
+        return response.data.artists.items
+      }));
+  }
+}
