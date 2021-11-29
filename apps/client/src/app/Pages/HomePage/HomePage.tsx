e7a512495b9bb520c846562b9ec474a51fc9114b
Clem <clement.bolin@epitech.eu>
2021-11-29 17:20:40 +0100

    add(feat/spotify_widget): spotify basic widget

--- apps/client/src/app/Pages/HomePage/HomePage.tsx
+++ apps/client/src/app/Pages/HomePage/HomePage.tsx
@@ -15,6 +15,7 @@ import { TopBar } from '../../Components/TopBar/Topbar';
 import { GithubFollowers } from '../../Components/Github/Followers/GithubFollowers';
 import { GithubRepositorys } from '../../Components/Github/Repository/GithubRepo';
 import { GithubProfile } from '../../Components/Github/Profil/Profil';
+import { SpotifyArtist } from '../../Components/Spotify/SpotifyArtist/SpotifyArtist';
 
 const Container = styled.div`
   padding-left: 500px;
@@ -41,7 +42,12 @@ export const HomePage = () => {
           <div>
             <Widget icon={AiFillGithub} count="Commit" title="Github Services" child={GithubFollowers} />
           </div>
-          </Draggable>
+        </Draggable>
+        <Draggable>
+          <div>
+            <Widget icon={BsSpotify} count="Artist" title="Artist" child={SpotifyArtist} />
+          </div>
+        </Draggable>
       </Container>
     </>
   )
