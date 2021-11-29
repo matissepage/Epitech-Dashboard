5148578ba45b30c5a189ad37461b674f1e7e8b10
Clem <clement.bolin@epitech.eu>
2021-11-29 21:02:51 +0100

    add(feat/spotify_widget): track interface

--- shared/spotify.model.ts
+++ shared/spotify.model.ts
@@ -19,3 +19,48 @@ export interface SpotifyArtist {
   type: 'track' | 'artist';
   uri: string;
 };
+
+export interface SpotifyTrack {
+  album: {
+    artists: {
+      external_urls: {
+        spotify: string;
+      },
+      href: string;
+      id: string;
+      uri: string;
+    }[];
+    available_markets: string[];
+    external_urls: {
+      spotify: string;
+    };
+    href: string;
+    id: string;
+    images: {
+      height: number;
+      url: string;
+      width: number;
+    }[];
+    name: string;
+    release_date: string;
+    total_tracks: string;
+    uri: string;
+  };
+  artists: {
+    external_urls: {
+      spotify: string;
+    };
+    id: string;
+    name: string;
+    uri: string;
+  }[];
+  duration_ms: Date;
+  explicit: string;
+  external_urls: {
+    spotify: string;
+  };
+  name: string;
+  popularity: number;
+  track_number: number;
+  uri: string;
+};
